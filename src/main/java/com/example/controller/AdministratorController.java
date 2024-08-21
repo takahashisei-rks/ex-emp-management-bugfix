d8592b735dc92c95f92b4fd7cae81dc9c5d5a416
takahashisei-rks <sei.takahashi@rakus-partners.co.jp>
2024-08-21 17:55:00 +0900

    feature/1ver3

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -3,7 +3,6 @@ package com.example.controller;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -92,10 +91,6 @@ public class AdministratorController {
             return toInsert();
         }
 
-		// if (!form.getPassword().equals(form.getConfirmPassword())) {
-        // 	model.addAttribute("errorMessage", "パスワードが一致しません。");
-        // 	return toInsert();
-    	// }
 		Administrator administrator = new Administrator();
 		// フォームからドメインにプロパティ値をコピー
 		BeanUtils.copyProperties(form, administrator);
