5c55c83dd7552d3261afd9d19154ff03645a1b53
takahashisei-rks <sei.takahashi@rakus-partners.co.jp>
2024-08-21 16:20:55 +0900

    feature/1

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -3,6 +3,9 @@ package com.example.controller;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -72,7 +75,15 @@ public class AdministratorController {
 	 * @return ログイン画面へリダイレクト
 	 */
 	@PostMapping("/insert")
-	public String insert(InsertAdministratorForm form) {
+	public String insert(@Validated InsertAdministratorForm form ,BindingResult result,Model model) {
+
+		if(result.hasErrors()){
+			return toInsert();
+		}
+		if (!form.getPassword().equals(form.getConfirmPassword())) {
+        	model.addAttribute("errorMessage", "パスワードが一致しません。");
+        	return toInsert();
+    	}
 		Administrator administrator = new Administrator();
 		// フォームからドメインにプロパティ値をコピー
 		BeanUtils.copyProperties(form, administrator);
